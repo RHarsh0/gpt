6588b515ca50e3f2457c5ac4871a82a408cf2b90
RHarsh0 <119388397+RHarsh0@users.noreply.github.com>
2023-10-15 17:41:47 +0530

    about section and features compleated and fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/logo.png'
 const Menu = () =>(
   <>
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt">About GPT</a></p>
+          <p><a href="#gpt">About GPT</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>

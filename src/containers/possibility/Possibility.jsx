bf7c44a16f85bb32d85c4dfebaf6c321a2bb20c5
RHarsh0 <119388397+RHarsh0@users.noreply.github.com>
2023-10-28 00:30:31 +0530

    possibilites and get started section done

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './Possibility.css';
+import Image from '../../assets/Image.png';
 const Possibility = () => {
   return (
-    <div>
+    <div className='gpt__possibility section__padding'>
+      <div className='gpt__possibility-image'>
+        <img src={Image} alt="possibility image" />
+      </div>
+      <div className='gpt__possibility-content'>
+        <h4>Request early access to get started</h4>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
       Possibility
     </div>
   )

6e295d84ea54a13d28130683ae1fcecd5c7323dc
RHarsh0 <119388397+RHarsh0@users.noreply.github.com>
2023-10-28 14:14:03 +0530

    footer section added

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './Article.css';
-const Article = () => {
+const Article = ({image,date,heading}) => {
   return (
-    <div>
-      Articles
+    <div className='gpt__article'>
+      <div className='gpt__article-image'>
+        <img src={image} alt="" />
+      </div>
+      <div className='gpt__article-content'>
+      <div className='gpt__article-content_date'>
+        <p>{date}</p>
+      </div>
+      <div className='gpt__article-content_heading'>
+        <h3>{heading}</h3>
+      </div>
+      <div className='gpt__article-content_btn'>
+        <button>Read full article</button>
+      </div>
+    </div>
     </div>
   )
 }
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,8 @@
+import Rect1 from '../../assets/Rect1.png';
+import Rect2 from '../../assets/Rect2.png';
+import Rect3 from '../../assets/Rect3.png';
+import Rect4 from '../../assets/Rect4.png';
+import Rect5 from '../../assets/Rect5.png';
+export{
+    Rect1, Rect2, Rect3, Rect4, Rect5
+}
